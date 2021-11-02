0200be9d30705c4c5c8b5972b3217cd481921067
Iman Rajabi <iman.rjb@gmail.com>
2021-11-02 15:09:24 +0330

    add config file

--- src/BitcoinRpcServiceProvider.php
+++ src/BitcoinRpcServiceProvider.php
@@ -25,5 +25,15 @@ class BitcoinRpcServiceProvider extends ServiceProvider
     {
         $this->app->alias(\ImanRjb\BitcoinRpc\Services\BitcoinRpc\BitcoinRpc::class, 'BitcoinRpc');
         $this->app->register(\ImanRjb\BitcoinRpc\Services\BitcoinRpc\BitcoinRpcServiceProvider::class);
+
+        $this->publishes([
+            __DIR__.'/config/bitcoin.php' => lumen_config_path('bitcoin.php'),
+        ], 'bitcoin-rpc');
+
+        if (file_exists($this->app->basePath() . '/config/bitcoin.php')) {
+            $this->mergeConfigFrom($this->app->basePath() . '/config/bitcoin.php', 'bitcoin');
+        } else {
+            $this->mergeConfigFrom(__DIR__ . '/config/bitcoin.php', 'bitcoin');
+        }
     }
 }
