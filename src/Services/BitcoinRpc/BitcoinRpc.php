21bbf3a1090313930c3d43dcb5a426a097611267
Iman Rajabi <iman.rjb@gmail.com>
2021-11-02 15:32:15 +0330

    add first method

--- src/Services/BitcoinRpc/BitcoinRpc.php
+++ src/Services/BitcoinRpc/BitcoinRpc.php
@@ -4,6 +4,10 @@ namespace ImanRjb\BitcoinRpc\Services\BitcoinRpc;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static getnewaddress($address_type = 'p2sh-segwit')
+ */
+
 class BitcoinRpc extends Facade
 {
     protected static function getFacadeAccessor()
--- src/Services/BitcoinRpc/BitcoinRpcService.php
+++ src/Services/BitcoinRpc/BitcoinRpcService.php
@@ -3,8 +3,19 @@
 namespace ImanRjb\BitcoinRpc\Services\BitcoinRpc;
 
 use Illuminate\Database\Eloquent\Model;
+use ImanRjb\BitcoinRpc\Bitcoin;
 
 class BitcoinRpcService
 {
+    private $bitcoin;
 
+    public function __construct()
+    {
+        $this->bitcoin = new Bitcoin();
+    }
+
+    public function getnewaddress($address_type = 'p2sh-segwit')
+    {
+        return $this->bitcoin->getnewaddress();
+    }
 }
