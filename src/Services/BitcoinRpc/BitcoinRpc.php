fd0c5051a9d50ad1af651369669938daacfeb473
Iman Rajabi <iman.rjb@gmail.com>
2021-11-03 09:13:19 +0330

    add all methods

--- src/Services/BitcoinRpc/BitcoinRpc.php
+++ src/Services/BitcoinRpc/BitcoinRpc.php
@@ -4,10 +4,6 @@ namespace ImanRjb\BitcoinRpc\Services\BitcoinRpc;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @method static getnewaddress($label = '', $address_type = 'p2sh-segwit')
- */
-
 class BitcoinRpc extends Facade
 {
     protected static function getFacadeAccessor()
